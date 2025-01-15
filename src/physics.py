1b06723cda4015fcc4d72f6a3add43422a3c2b3f
DaBear57 <ben.r.debear@gmail.com>
2025-01-14 22:21:17 -0500

    revamp physics.py (wip)

--- src/physics.py
+++ src/physics.py
@@ -1,13 +1,35 @@
 import math
 
 from phoenix6 import unmanaged
+from phoenix6.hardware.talon_fx import TalonFX
 from pyfrc.physics.core import PhysicsInterface
 from pyfrc.physics.drivetrains import four_motor_swerve_drivetrain
 from wpilib import DriverStation
 from wpilib.simulation import DCMotorSim
-from wpimath.system.plant import DCMotor
+from wpimath.system.plant import DCMotor, LinearSystemId
 
 from robot import MyRobot
+
+
+class FalconSim:
+    def __init__(self, motor: TalonFX, moi: float, gearing: float):
+        self.gearbox = DCMotor.falcon500(1)
+        self.plant = LinearSystemId.DCMotorSystem(self.motor_sim, moi, gearing)
+        self.gearing = gearing
+        self.sim_state = motor.sim_state
+        self.sim_state.set_supply_voltage(12.0)
+        self.motor_sim = DCMotorSim(self.plant, self.gearbox)
+
+    def update(self, dt: float):
+        voltage = self.sim_state.motor_voltage
+        self.motor_sim.setInputVoltage(voltage)
+        self.motor_sim.update(dt)
+        self.sim_state.set_raw_rotor_position(
+            self.motor_sim.getAngularPositionRotations() * self.gearing
+        )
+        self.sim_state.set_rotor_velocity(
+            self.motor_sim.getAngularVelocityRPM() / 60 * self.gearing
+        )
 
 
 class PhysicsEngine:
